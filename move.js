7fc42234c11756ef1904cf0847e3de1e613c7738
Vikas bisariya <vikasbisariya20@gmail.com>
2023-07-14 17:44:55 +0530

    updated all files

--- move.js
+++ move.js
@@ -11,14 +11,29 @@ document.querySelector(".img2").setAttribute("src","./images/dice"+ randomNumber
 
 if(randomNumber1 === randomNumber2){
     document.querySelector("h1").innerHTML = "Draw!";
+    document.getElementsByClassName("number1")[0].innerHTML = randomNumber1;
+    document.getElementsByClassName("number2")[0].innerHTML = randomNumber2;
 }
 else if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "Player 1 Wins";
+    document.getElementsByClassName("number1")[0].innerHTML = randomNumber1;
+    document.getElementsByClassName("number2")[0].innerHTML = randomNumber2;
 }
 
 else{
     document.querySelector("h1").innerHTML = "Player 2 Wins";
+    document.getElementsByClassName("number1")[0].innerHTML = randomNumber1;
+    document.getElementsByClassName("number2")[0].innerHTML = randomNumber2;
 }
+
+// to change color at each time when you press
+const colors = ["red", "black", "yellow", "pink", "white"];
+
+  const playerText = document.querySelectorAll(".player1");
+
+  playerText.forEach((element, index) => {
+    element.style.color = color[index % colors.length];
+  });
 }
 
 // to change the names
@@ -32,4 +47,7 @@ function editNames(){
     document.querySelector("h3.player1").innerHTML = player1;
     document.querySelector("h3.player2").innerHTML = player2;
 }
+
+
+
 
